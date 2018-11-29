9f264d00d89efef18e2cafad2308326b5df769e0
Alex Sandoval <alexsandovalv@gmail.com>
2018-11-29 18:47:38 -0500

    Add login libs

--- app/src/main/java/pe/edu/upc/warehouse/LoginActivity.java
+++ app/src/main/java/pe/edu/upc/warehouse/LoginActivity.java
@@ -1,13 +1,19 @@
 package pe.edu.upc.warehouse;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText email;
     private EditText password;
+    private Button btnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +22,45 @@ public class LoginActivity extends AppCompatActivity {
 
         email = findViewById(R.id.txtEmail);
         password = findViewById(R.id.txtPassword);
+        btnIngresar = findViewById(R.id.btnLogin);
 
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.length()==0 || password.length()==0)
+                {
+                    Toast.makeText(LoginActivity.this, "DEBE INGRESAR LAS CREDENCIALES", Toast.LENGTH_SHORT).show();
+                }else
+                {
+                    validarUsuario(email.getText().toString(),password.getText().toString());
+                }
+            }
+        });
 
+    }
 
+    public void validarUsuario(String email, String password){
+
+    }
+
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Deseas salir de la aplicación");
+        builder.setTitle("Mensaje...");
+        builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+                return;
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
--- app/src/main/java/pe/edu/upc/warehouse/util/Constants.java
+++ app/src/main/java/pe/edu/upc/warehouse/util/Constants.java
@@ -1,4 +1,14 @@
 package pe.edu.upc.warehouse.util;
 
 public class Constants {
+
+    static final String PREFERENCIAS_COMPARTIDAD= "sp_warehouse";
+
+    static final String ID_USUARIO ="IdUsuario";
+    static final String DNI_USUARIO = "DniUsuario";
+    static final String NOMBRE_USUARIO = "NombreUsuario";
+    static final String EMAIL_USUARIO = "EmailUsuario";
+
+    static final String SERVER_URL = BackendSetting.getServerUrl();
+
 }
